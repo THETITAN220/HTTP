1a748157dc0f541384a85759a139c9b1d881e2ff
THETITAN220 <raunakrose15@gmail.com>
2025-12-15 20:59:24 +0530

    HTTP header validation

--- request/request.go
+++ request/request.go
@@ -3,6 +3,7 @@ package request
 import (
 	"fmt"
 	"io"
+	"strings"
 )
 
 type RequestLine struct {
@@ -11,14 +12,42 @@ type RequestLine struct {
 	Method        string
 }
 
+func (r *RequestLine) validHTTP() bool {
+	return r.HttpVersion == "HTTP/1.1"
+}
+
 type Request struct {
 	RequestLine RequestLine
 }
 
 var ERROR_BAD_REQUEST_HEADER = fmt.Errorf("Bad Request Header")
+var ERROR_UNSUPPORTED_HTTP = fmt.Errorf("Invalid HTTP version in header")
+var SEPARATOR = "\r\n"
 
-func parseRequestLine(b []byte) (*RequestLine, string, error) {
+func parseRequestLine(b string) (*RequestLine, string, error) {
+	index := strings.Index(b, SEPARATOR)
+	if index != -1 {
+		return nil, b, nil
+	}
 
+	startLine := b[:index]
+	restOfMsg := b[index+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, restOfMsg, ERROR_BAD_REQUEST_HEADER
+	}
+
+	reqLine := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   parts[2],
+	}
+
+	if !reqLine.validHTTP() {
+		return nil, restOfMsg, ERROR_UNSUPPORTED_HTTP
+	}
+	return reqLine, restOfMsg, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
