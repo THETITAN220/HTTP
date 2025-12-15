a372cb25000b4522e1a5627ef54308d9760c52a3
THETITAN220 <raunakrose15@gmail.com>
2025-12-15 21:51:39 +0530

    changes in HTTP version validation

--- request/request.go
+++ request/request.go
@@ -13,16 +13,13 @@ type RequestLine struct {
 	Method        string
 }
 
-func (r *RequestLine) validHTTP() bool {
-	return r.HttpVersion == "HTTP/1.1"
-}
-
 type Request struct {
 	RequestLine RequestLine
 }
 
 var ERROR_BAD_REQUEST_HEADER = fmt.Errorf("Bad Request Header")
-var ERROR_UNSUPPORTED_HTTP = fmt.Errorf("Invalid HTTP version in header")
+var ERROR_UNSUPPORTED_HTTP = fmt.Errorf("Invalid HTTP in header")
+var ERROR_UNSUPPORTED_HTTP_VER = fmt.Errorf("Invalid HTTP version in header")
 var SEPARATOR = "\r\n"
 
 func parseRequestLine(b string) (*RequestLine, string, error) {
@@ -39,16 +36,19 @@ func parseRequestLine(b string) (*RequestLine, string, error) {
 		return nil, restOfMsg, ERROR_BAD_REQUEST_HEADER
 	}
 
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfMsg, ERROR_UNSUPPORTED_HTTP_VER
+	}
+
 	reqLine := &RequestLine{
 		Method:        parts[0],
 		RequestTarget: parts[1],
-		HttpVersion:   parts[2],
+		HttpVersion:   httpParts[1],
 	}
 
-	if !reqLine.validHTTP() {
-		return nil, restOfMsg, ERROR_UNSUPPORTED_HTTP
-	}
 	return reqLine, restOfMsg, nil
+
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
