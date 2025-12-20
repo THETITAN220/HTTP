acce4fe8d162402c552432f8de283b0bcc8ad060
THETITAN220 <raunakrose15@gmail.com>
2025-12-20 11:43:55 +0530

    FEATURE: Added Field Line parsing

---
+++ internal/headers/headers.go
@@ -0,0 +1,58 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Headers map[string]string
+
+var CRLF = []byte("\r\n")
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("Malformed field line")
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("Malformed field name")
+	}
+
+	return string(name), string(value), nil
+
+}
+
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+	for {
+		idx := bytes.Index(data[read:], CRLF)
+		if idx == -1 {
+			break
+		}
+
+		// EMPTY HEADER
+		if idx == 0 {
+			done = true
+			break
+		}
+
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+		read += idx + len(CRLF)
+		h[name] = value
+
+	}
+
+	return read, done, nil
+}
