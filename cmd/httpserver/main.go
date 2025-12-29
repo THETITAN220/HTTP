20c5e6417d27d3ee1f988cb8a408a7ae89a04a74
THETITAN220 <raunakrose15@gmail.com>
2025-12-29 23:10:18 +0530

    FEATURE: Response server is now active

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,21 +1,88 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/THETITAN220/HTTP/internal/request"
+	"github.com/THETITAN220/HTTP/internal/response"
 	"github.com/THETITAN220/HTTP/internal/server"
 )
 
 const port = 42069
 
+func respond400() []byte {
+	return []byte(`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`)
+}
+
+func respond500() []byte {
+	return []byte(`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`)
+}
+
+func respond200() []byte {
+	return []byte(`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`)
+}
+
 func main() {
 
-	s, err := server.Serve(port)
+	s, err := server.Serve(port, func(w *response.Writer, req *request.Request) {
+		h := response.GetDefaultHeaders(0)
+		body := respond200()
+		status := response.StatusOk
+
+		switch req.RequestLine.RequestTarget {
+		case "/yourproblem":
+			status = response.StatusBadRequest
+			body = respond400()
+		case "/myproblem":
+			status = response.StatusInternalServerError
+			body = respond500()
+		}
+
+		// if req.RequestLine.RequestTarget == "/yourproblem" {
+		// 	status = response.StatusBadRequest
+		// 	body = respond400()
+
+		// } else if req.RequestLine.RequestTarget == "/myproblem" {
+		// 	status = response.StatusInternalServerError
+		// 	body = respond500()
+		// }
+
+		w.WriteStatusLine(status)
+		h.Replace("Content-length", fmt.Sprintf("%d", len(body)))
+		h.Replace("Content-type", "text/html")
+		w.WriteHeaders(*h)
+		w.WriteBody(body)
+	})
 	if err != nil {
-		log.Fatal("Error starting server: %v\n", err)
+		log.Printf("Error starting server: %v\n", err)
 	}
 
 	defer s.Close()
